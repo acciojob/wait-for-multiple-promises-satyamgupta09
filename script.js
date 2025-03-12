925728ab62c9d769bc0e00795acc4be4897d317d
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-12 23:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-let prom1 = new Promise((resolve, reject)=>{
+let prom1 = new Promise((resolve)=>{
 	setTimeout(()=>{
-		return resolve({name:'Promise1', time:1000});
+		resolve({data:1,time:500});
 	},1000);
-});
+})
 
-let prom2=new Promise((resolve, reject)=>{
+let prom2 = new Promise((resolve)=>{
 	setTimeout(()=>{
-		return resolve({name:'Promise2', time:500});
+		resolve({data:2,time:1000});
+	},1000);
+})
+
+let prom3 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve({data:3, time:1000});
 	},2000);
-});
+})
 
-let prom3=new Promise((resolve, reject)=>{
-	setTimeout(()=>{
-		return resolve({name:'Promise3', time:700});
-	},3000);
-});
+document.getElementById("output").innerHTML='Loading...';
 
-document.getElementById('output').innerHTML += `
-            <tr id="loading">
-                <td colspan=2>Loading...</td>
-            </tr>
-          `;
 
 Promise.all([prom1, prom2, prom3]).then((res)=>{
-	document.getElementById('output').innerHTML=``;
-	res.forEach((data)=>{
-		document.getElementById('output').innerHTML+=`
-		  <tr>
-		    <td>${data.name}</td>
-			<td>${data.time}</td>
-		  </tr>
-		`;
+	document.getElementById("output").innerHTML='';
+	
+
+	res.forEach((item)=>{
+		document.getElementById("output").innerHTML+=`
+		<tr>
+		 <td>Promise ${item.data}</td>
+		 <td>${item.time}</td>
+		</tr>
+		`
 	});
 
-	let totalTime = res.reduce((sum, data)=>sum + data.time,0)/1000;
+	let total=0;
+	res.forEach((item)=>total+=item.time)
 
-    document.getElementById('output').innerHTML+=`
-		  <tr>
-		    <td>Total Time</td>
-			<td>${totalTime.toFixed(3)}</td>
-		  </tr>
-		`;
-});
+	document.getElementById("output").innerHTML+=`
+		<tr>
+		 <td>Total</td>
+		 <td>${total}</td>
+		</tr>
+		`
+})
